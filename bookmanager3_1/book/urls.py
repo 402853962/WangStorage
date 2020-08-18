c5f890edd7df49c1e27d5ab99e5d684b9938d80a
王子潇 <wangzixiao@itcast.com>
2020-08-18 21:33:12 +0800

    8.18round1

--- bookmanager3_1/book/urls.py
+++ bookmanager3_1/book/urls.py
@@ -1,14 +1,35 @@
 from django.urls import path
-from book.views import index,login,book,login_json,header,detail,jsonresponse,to_index
+from book.views import index
+from book import views
+from django.urls import converters
 # book,login_json,header,detail,jsonresponse,to_index
+
+
+class MobileConverter:
+    regex='1[3-9]\d{9}'
+
+    def to_python(self,value):
+        return value
+
+converters.register_converter(MobileConverter,'mobile')
 
 urlpatterns = [
     path('index/',index),
-    path('login/', login),
-    path('<cat_id>/<book_id>/',book),
-    path('login_json/', login_json),
-    path('header/', header),
-    path('detail/', detail),
-    path('jsonresponse/', jsonresponse),
-    path('to_index/', to_index)
+    # path('login/', login),
+    # path('<cat_id>/<book_id>/',book),
+    # path('login_json/', login_json),
+    # path('header/', header),
+    # path('detail/', detail),
+    # path('jsonresponse/', jsonresponse),
+    # path('to_index/', to_index),
+    path('p/<int:tieba_id>/', views.baidu_tieba),
+    path('register/<mobile:phone>/', views.tieba_register),
+    path('tieba_login/', views.tieba_login),
+    path('get_cookies/', views.get_cookies),
+    path('set_session/', views.set_session),
+    path('get_session/', views.get_session),
+    path('jd_register/', views.jd_register),
+
+    path('meiduo_register/',views.RegisterView.as_view()),
+    path('center/',views.CenterView.as_view())
 ]
--- bookmanager3_1/book/views.py
+++ bookmanager3_1/book/views.py
@@ -97,50 +97,50 @@ def index(request):
 # def to_index(request):
 #     return redirect("http://www.baidu.com/")
 
-def login(request):
-    print(request.POST)
-    return HttpResponse('login')
-
-def book(request,cat_id,book_id):
-    keyword = request.GET.getlist('keyword')
-    return HttpResponse(keyword)
-
-import json
-def login_json(request):
-    body = request.body
-    body_str = body.decode()
-    body_dict = json.loads(body_str)
-    print(body_dict)
-    return HttpResponse('json')
-
-def header(request):
-    if request.method == 'GET':
-        print('GET')
-    elif request.method == 'POST':
-        print('POST')
-    else:
-        print(request.method)
-    head = request.META
-    print(head)
-    return HttpResponse('header')
-
-def detail(request):
-    result = HttpResponse(content='detail',content_type='text/http',status=200)
-    result['name'] = 'jack'
-    return result
-
-from django.http import JsonResponse
-def jsonresponse(request):
-    info = {
-        'name' : 'jack',
-        'password' : 123456
-    }
-    return JsonResponse(info)
-
-from django.shortcuts import redirect
-def to_index(request):
-    return redirect('http://www.qq.com')
-
+# def login(request):
+#     print(request.POST)
+#     return HttpResponse('login')
+#
+# def book(request,cat_id,book_id):
+#     keyword = request.GET.getlist('keyword')
+#     return HttpResponse(keyword)
+#
+# import json
+# def login_json(request):
+#     body = request.body
+#     body_str = body.decode()
+#     body_dict = json.loads(body_str)
+#     print(body_dict)
+#     return HttpResponse('json')
+#
+# def header(request):
+#     if request.method == 'GET':
+#         print('GET')
+#     elif request.method == 'POST':
+#         print('POST')
+#     else:
+#         print(request.method)
+#     head = request.META
+#     print(head)
+#     return HttpResponse('header')
+#
+# def detail(request):
+#     result = HttpResponse(content='detail',content_type='text/http',status=200)
+#     result['name'] = 'jack'
+#     return result
+#
+# from django.http import JsonResponse
+# def jsonresponse(request):
+#     info = {
+#         'name' : 'jack',
+#         'password' : 123456
+#     }
+#     return JsonResponse(info)
+#
+# from django.shortcuts import redirect
+# def to_index(request):
+#     return redirect('http://www.qq.com')
+#
 
 
 
@@ -149,10 +149,77 @@ def to_index(request):
 from book.models import BookInfo,PeopleInfo
 from django.core.paginator import Paginator
 
-BookInfo.objects.filter(peopleinfo__name='郭靖')
-BookInfo.objects.filter(peopleinfo__description__contains='八')
-PeopleInfo.objects.filter(book__name='天龙八部')
-PeopleInfo.objects.filter(book__readcount__gt='30')
+# BookInfo.objects.filter(peopleinfo__name='郭靖')
+# BookInfo.objects.filter(peopleinfo__description__contains='八')
+# PeopleInfo.objects.filter(book__name='天龙八部')
+# PeopleInfo.objects.filter(book__readcount__gt='30')
 # people = PeopleInfo.objects.all()[0:9]
 # paginator = Paginator(people,2)
 # page_people = paginator.page(2)
+
+
+
+######################################################################
+######################################################################
+def baidu_tieba(request,tieba_id):
+    return HttpResponse(tieba_id)
+
+def tieba_register(request,phone):
+    return HttpResponse(phone)
+
+def tieba_login(request):
+    username = request.GET.get('username')
+    result = HttpResponse('set_cookie')
+    result.set_cookie(key='username',value=username,max_age=30)
+    return result
+
+def get_cookies(request):
+
+    print(request.COOKIES)
+    username = request.COOKIES.get('username')
+    return HttpResponse(username)
+
+def set_session(request):
+    request.session['user_id'] =15423132312
+    return HttpResponse('set_session')
+
+def get_session(request):
+    user_id = request.session['user_id']
+    #删除数据
+    request.session.clear()
+    #删除数据以及key
+    request.session.flush()
+    #设置过期时间,None为默认，两周
+    request.session.set_expiry(30)
+    return HttpResponse(user_id)
+
+def jd_register(request):
+    if request.method == 'GET':
+        print('GET')
+    elif request.method == 'POST':
+        print('POST')
+    else:
+        return HttpResponse('other')
+
+    return HttpResponse('jd_register')
+
+from django.views import View
+class RegisterView(View):
+    def get(self,request):
+        return HttpResponse('view get')
+    def post(self,request):
+        return  HttpResponse('view post')
+    def other(self,request):
+        pass
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+class CenterView(LoginRequiredMixin,View):
+    def  get(self,request):
+        return HttpResponse('个人中心 get')
+    def post(self,request):
+        return HttpResponse('个人中心 post')
+
+
+
+
+
